98135f2438ce71e88993b93fef184b2e438dfe4f
Ubuntu <ubuntu@ip-172-31-30-130.ap-northeast-1.compute.internal>
2023-05-14 06:12:08 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root "application#hello"
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
